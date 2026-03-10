d7a1e11f6a458c25fafa1cd55f80324e67910aa8
Giovana Correia <vitoriagomescorreia2016@gmail.com>
2026-03-10 13:30:21 -0300

    botão salvar cadastrando produto no banco

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,8 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
+
 
 
 public class ProdutosDAO {
@@ -23,51 +25,34 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-       String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+     
+    String sql = "INSERT INTO produtos(nome, valor, status) VALUES (?, ?, ?)";
 
-        try {
+    conn = new conectaDAO().connectDB();
 
-            conn = new conectaDAO().connectDB();
-            prep = conn.prepareStatement(sql);
+    try{
 
-            prep.setString(1, produto.getNome());
-            prep.setInt(2, produto.getValor());
-            prep.setString(3, produto.getStatus());
+        prep = conn.prepareStatement(sql);
 
-            prep.execute();
-            prep.close();
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
 
-        } catch (Exception erro) {
-            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro);
-        }
+        prep.executeUpdate();
+
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+    } catch (SQLException erro){
+
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro.getMessage());
+
+    }
+     
         
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-     String sql = "SELECT * FROM produtos";
-
-        try {
-
-            conn = new conectaDAO().connectDB();
-            prep = conn.prepareStatement(sql);
-            resultset = prep.executeQuery();
-
-            while(resultset.next()){
-
-                ProdutosDTO produto = new ProdutosDTO();
-
-                produto.setId(resultset.getInt("id"));
-                produto.setNome(resultset.getString("nome"));
-                produto.setValor(resultset.getInt("valor"));
-                produto.setStatus(resultset.getString("status"));
-
-                listagem.add(produto);
-            }
-
-        } catch (Exception erro){
-            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro);
-        }   
+     
        
     
     return listagem;
