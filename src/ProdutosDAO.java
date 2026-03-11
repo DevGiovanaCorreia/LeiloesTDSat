b67980a0a5f3d97912221d05715ca9f4b147f74b
Giovana Correia <vitoriagomescorreia2016@gmail.com>
2026-03-11 14:30:17 -0300

    implementada função venderProduto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -85,6 +85,28 @@ public class ProdutosDAO {
     return listagem;
     }
     
+    public void venderProduto(int id){
+
+    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+    conn = new conectaDAO().connectDB();
+
+    try{
+
+        prep = conn.prepareStatement(sql);
+        prep.setInt(1, id);
+
+        prep.executeUpdate();
+
+        JOptionPane.showMessageDialog(null,"Produto vendido com sucesso");
+
+    } catch (SQLException erro){
+
+        JOptionPane.showMessageDialog(null,"Erro ao vender produto");
+
+    }
+
+}
     
     
         
